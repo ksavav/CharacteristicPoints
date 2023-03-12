01d8e2865fbbec629fe330da75b3736b08b80179
Ksawery Armatynski <a.xawery@gmail.com>
2023-03-12 01:27:59 +0100

    First stage of UI

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CharacteristicPoints
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitButton(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void DragWindow(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void UploadImage(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            od.Filter = "Image files|*.bmp;*.jpg;*.png";
+            od.FilterIndex = 1;
+
+            if (od.ShowDialog() == true)
+            {
+                UserImage.Source = new BitmapImage(new Uri(od.FileName));
+                upload.Visibility= Visibility.Hidden;
+                border.Visibility= Visibility.Visible;
+            }
+        }
+    }
+}
